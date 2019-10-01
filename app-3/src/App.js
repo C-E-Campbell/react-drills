f78f28cf9d849b06bc0442f5855891c806b242aa
charles <codelyfewithcharlie@gmail.com>
2019-10-01 14:18:03 -0700

    finijshed problem 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,19 +3,44 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.filterArray = this.filterArray.bind(this);
+		this.state = {
+			testArray: ["red", "blue", "green"],
+			filteredArray: []
+		};
+	}
+
+	filterArray(e) {
+		const filtered = this.state.testArray.filter(element => {
+			return element.includes(e);
+		});
+		this.setState({ filteredArray: filtered });
+	}
+
+	render() {
+		const displayFiltered = this.state.filteredArray.map((element, i) => {
+			return <h4 key={i}>{element}</h4>;
+		});
+
+		const unfiltered = this.state.testArray.map((element, i) => {
+			return <h4 key={i}>{element}</h4>;
+		});
+
+		return (
+			<div className='App'>
+				<input
+					onChange={e => {
+						this.filterArray(e.target.value);
+					}}
+				/>
+				<div>
+					{this.state.filteredArray.length > 0 ? displayFiltered : unfiltered}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
