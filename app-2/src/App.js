b8b1504afa04e47a5ec6431a3ef62eca96810f60
charles <codelyfewithcharlie@gmail.com>
2019-10-01 13:42:21 -0700

    finijshed problem 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,19 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			newArray: ["red", "blue", "orange", "purple"]
+		};
+	}
+	render() {
+		const mappedArray = this.state.newArray.map(element => {
+			return <h3>{element}</h3>;
+		});
+		return <div className='App'>{mappedArray}</div>;
+	}
 }
 
 export default App;
